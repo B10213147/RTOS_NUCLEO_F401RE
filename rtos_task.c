088c5cc40a2da312260c5ce5e3d30a35c33a5390
B10213147 <B10213147@yuntech.org.tw>
2017-01-24 23:22:26 +0800

    Added two functions

--- rtos_task.c
+++ rtos_task.c
@@ -5,11 +5,27 @@
  *      Author: Harvard Tseng
 **/
  
-#include "rtos.h"
+#include <stdlib.h>
+#include "rtos_task.h"
 
 void empty(void){
 }
-
+	
 voidfuncptr priv_task = empty;
 voidfuncptr sch_tab[] = {empty};
 int sch_tab_length = sizeof(sch_tab) / sizeof(sch_tab[0]);
+
+struct rtos_task *rtos_running_task;
+struct rtos_task *rtos_ready_tasks = NULL;
+
+void rtos_task_create(voidfuncptr func, void *arg){
+	struct rtos_task *task = (struct rtos_task *)malloc(sizeof(struct rtos_task));
+	rtos_task_insert(&rtos_ready_tasks, task);
+	task->function = func;
+	task->arg = arg;
+}
+
+void rtos_task_insert(struct rtos_task **list, struct rtos_task *task){
+	task->next = *list;
+	*list = task;
+}
