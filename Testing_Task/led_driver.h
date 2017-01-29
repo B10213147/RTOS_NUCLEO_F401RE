b6549ea6941fc754fddfb4da3ffd59e714f441f4
B10213147 <B10213147@yuntech.org.tw>
2017-01-29 19:31:49 +0800

    Added four tasks

--- Testing_Task/led_driver.h
+++ Testing_Task/led_driver.h
@@ -13,7 +13,11 @@
 #endif /* __cplusplus */
 
 void led_driver_init(void);
-void led_driver(void); 
+void led_driver(void);
+void task1(void);	 
+void task2(void);
+void task3(void);
+void task4(void);
 	 
 #ifdef __cplusplus
 }
