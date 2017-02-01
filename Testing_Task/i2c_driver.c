7c18e126bf9e73f9a4e11d6c47435ea12385f736
B10213147 <B10213147@yuntech.org.tw>
2017-02-01 18:14:48 +0800

    Test1 success
    Master_Transmitter 2 Slave_Receiver.

--- Testing_Task/i2c_driver.c
+++ Testing_Task/i2c_driver.c
@@ -12,46 +12,48 @@
 
 void i2c_driver_init(void){
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_I2C1 | RCC_APB1Periph_I2C2, ENABLE);
-
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_I2C1 | RCC_APB1Periph_I2C2, ENABLE);	
+	
 	//
-	// Enable the GPIO pin for the I2C1(PB8, PB9), I2C2(PB3, PB10).
+	// Enable the GPIO pin for the I2C1(PB8, PB9), I2C2(PB3, PB10). 
 	// Set the mode as Alternate function.
-	//
+	//	
 	GPIO_PinAFConfig(GPIOB, GPIO_PinSource8, GPIO_AF_I2C1);
 	GPIO_PinAFConfig(GPIOB, GPIO_PinSource9, GPIO_AF_I2C1);
 	GPIO_PinAFConfig(GPIOB, GPIO_PinSource3, GPIO_AF9_I2C2);
 	GPIO_PinAFConfig(GPIOB, GPIO_PinSource10, GPIO_AF_I2C2);
-
+	
 	GPIO_InitTypeDef GPIO_InitStruct;
 	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_3 | GPIO_Pin_8 | GPIO_Pin_9 | GPIO_Pin_10;
 	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;
+	GPIO_InitStruct.GPIO_OType = GPIO_OType_OD;
 	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;
 	GPIO_Init(GPIOB, &GPIO_InitStruct);
-
+	
 	I2C_InitTypeDef I2C_InitStruct;
-	I2C_InitStruct.I2C_ClockSpeed = 5000;
+	I2C_InitStruct.I2C_ClockSpeed = 100000;
 	I2C_InitStruct.I2C_Mode = I2C_Mode_I2C;
 	I2C_InitStruct.I2C_DutyCycle = I2C_DutyCycle_2;
-	I2C_InitStruct.I2C_OwnAddress1 = 0;
+	I2C_InitStruct.I2C_OwnAddress1 = 0xA0;
 	I2C_InitStruct.I2C_Ack = I2C_Ack_Enable;
 	I2C_InitStruct.I2C_AcknowledgedAddress = I2C_AcknowledgedAddress_7bit;
 	I2C_Init(I2C1, &I2C_InitStruct);
-
-	I2C_InitStruct.I2C_OwnAddress1 = 0x10 << 1;
+	
+	I2C_InitStruct.I2C_OwnAddress1 = 0x30;
 	I2C_Init(I2C2, &I2C_InitStruct);
-
+	
 	I2C_Cmd(I2C1, ENABLE);
 	I2C_Cmd(I2C2, ENABLE);
 }
 
 void i2c_Master_Transmitter(void){
 	I2C_GenerateSTART(I2C1, ENABLE);
-	while(!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_MODE_SELECT));
-	I2C_Send7bitAddress(I2C1, 0x10, I2C_Direction_Transmitter);
-	while(!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED));
-	I2C_SendData(I2C1, 'M');
-	while(!I2C_GetFlagStatus(I2C1, I2C_FLAG_BTF));
+	if(I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_MODE_SELECT)){
+		I2C_Send7bitAddress(I2C1, 0x10, I2C_Direction_Transmitter);
+		while(!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED));
+		I2C_SendData(I2C1, 'M');
+		while(!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_TRANSMITTED));
+	}
 	I2C_GenerateSTOP(I2C1, ENABLE);
 }
 
@@ -73,10 +75,38 @@ void i2c_Slave_Transmitter(void){
 	while(!I2C_CheckEvent(I2C2, I2C_EVENT_SLAVE_ACK_FAILURE));
 }
 
-char slave_Rx;
+char slave_Rx[2];
 void i2c_Slave_Receiver(void){
+	if(I2C_CheckEvent(I2C2, I2C_EVENT_SLAVE_RECEIVER_ADDRESS_MATCHED)){
+		while(!I2C_CheckEvent(I2C2, I2C_EVENT_SLAVE_BYTE_RECEIVED));
+		slave_Rx[1] = I2C_ReceiveData(I2C2);
+		while(!I2C_CheckEvent(I2C2, I2C_EVENT_SLAVE_STOP_DETECTED));
+	}
+}
+
+void test1(void){
+	slave_Rx[0] = 0; slave_Rx[1] = 0;
+	I2C_GenerateSTART(I2C1, ENABLE);
+	while(!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_MODE_SELECT));
+	I2C_Send7bitAddress(I2C1, 0x30, I2C_Direction_Transmitter);
+	
 	while(!I2C_CheckEvent(I2C2, I2C_EVENT_SLAVE_RECEIVER_ADDRESS_MATCHED));
+	
+	while(!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED));
+	I2C_SendData(I2C1, 'A');	
+	while(!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_TRANSMITTING));
+	
 	while(!I2C_CheckEvent(I2C2, I2C_EVENT_SLAVE_BYTE_RECEIVED));
-	slave_Rx = I2C_ReceiveData(I2C2);
+	slave_Rx[0] = I2C_ReceiveData(I2C2);
+
+	I2C_SendData(I2C1, 'B');	
+	
+	while(!I2C_CheckEvent(I2C2, I2C_EVENT_SLAVE_BYTE_RECEIVED));
+	slave_Rx[1] = I2C_ReceiveData(I2C2);
+	
+	while(!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_TRANSMITTED));
+	I2C_GenerateSTOP(I2C1, ENABLE);
+	
 	while(!I2C_CheckEvent(I2C2, I2C_EVENT_SLAVE_STOP_DETECTED));
 }
+
--- Testing_Task/i2c_driver.h
+++ Testing_Task/i2c_driver.h
@@ -17,6 +17,7 @@ void i2c_Master_Transmitter(void);
 void i2c_Master_Receiver(void);
 void i2c_Slave_Transmitter(void);
 void i2c_Slave_Receiver(void);
+void test1(void);
 	 
 #ifdef __cplusplus
 }
