85eb3b1209c7e73b48ed9afda3c782f4dd7d6a2b
B10213147 <B10213147@yuntech.org.tw>
2017-02-01 11:27:32 +0800

    Finished three functions(untested)
    i2c_Master_Receiver()
    i2c_Slave_Transmitter()
    i2c_Slave_Receiver()

--- Testing_Task/i2c_driver.c
+++ Testing_Task/i2c_driver.c
@@ -12,23 +12,23 @@
 
 void i2c_driver_init(void){
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_I2C1 | RCC_APB1Periph_I2C2, ENABLE);	
-	
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_I2C1 | RCC_APB1Periph_I2C2, ENABLE);
+
 	//
-	// Enable the GPIO pin for the I2C1(PB8, PB9), I2C2(PB3, PB10). 
+	// Enable the GPIO pin for the I2C1(PB8, PB9), I2C2(PB3, PB10).
 	// Set the mode as Alternate function.
-	//	
+	//
 	GPIO_PinAFConfig(GPIOB, GPIO_PinSource8, GPIO_AF_I2C1);
 	GPIO_PinAFConfig(GPIOB, GPIO_PinSource9, GPIO_AF_I2C1);
 	GPIO_PinAFConfig(GPIOB, GPIO_PinSource3, GPIO_AF9_I2C2);
 	GPIO_PinAFConfig(GPIOB, GPIO_PinSource10, GPIO_AF_I2C2);
-	
+
 	GPIO_InitTypeDef GPIO_InitStruct;
 	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_3 | GPIO_Pin_8 | GPIO_Pin_9 | GPIO_Pin_10;
 	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;
 	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;
 	GPIO_Init(GPIOB, &GPIO_InitStruct);
-	
+
 	I2C_InitTypeDef I2C_InitStruct;
 	I2C_InitStruct.I2C_ClockSpeed = 5000;
 	I2C_InitStruct.I2C_Mode = I2C_Mode_I2C;
@@ -37,10 +37,10 @@ void i2c_driver_init(void){
 	I2C_InitStruct.I2C_Ack = I2C_Ack_Enable;
 	I2C_InitStruct.I2C_AcknowledgedAddress = I2C_AcknowledgedAddress_7bit;
 	I2C_Init(I2C1, &I2C_InitStruct);
-	
+
 	I2C_InitStruct.I2C_OwnAddress1 = 0x10 << 1;
 	I2C_Init(I2C2, &I2C_InitStruct);
-	
+
 	I2C_Cmd(I2C1, ENABLE);
 	I2C_Cmd(I2C2, ENABLE);
 }
@@ -51,17 +51,32 @@ void i2c_Master_Transmitter(void){
 	I2C_Send7bitAddress(I2C1, 0x10, I2C_Direction_Transmitter);
 	while(!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED));
 	I2C_SendData(I2C1, 'M');
-	while(!I2C_GetFlagStatus(I2C1,I2C_FLAG_BTF));
+	while(!I2C_GetFlagStatus(I2C1, I2C_FLAG_BTF));
 	I2C_GenerateSTOP(I2C1, ENABLE);
 }
 
+char master_Rx;
 void i2C_Master_Receiver(void){
-	
+	I2C_GenerateSTART(I2C1, ENABLE);
+	while(!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_MODE_SELECT));
+	I2C_Send7bitAddress(I2C1, 0x10, I2C_Direction_Receiver);
+	while(!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_RECEIVER_MODE_SELECTED));
+	while(!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_RECEIVED));
+	master_Rx = I2C_ReceiveData(I2C1);
+	I2C_GenerateSTOP(I2C1, ENABLE);
 }
 
 void i2c_Slave_Transmitter(void){
-	
+	while(!I2C_CheckEvent(I2C2, I2C_EVENT_SLAVE_TRANSMITTER_ADDRESS_MATCHED));
+	I2C_SendData(I2C2, 'S');
+	while(!I2C_CheckEvent(I2C2, I2C_EVENT_SLAVE_BYTE_TRANSMITTED));
+	while(!I2C_CheckEvent(I2C2, I2C_EVENT_SLAVE_ACK_FAILURE));
 }
-void i2c_Slave_Reciver(void){
-	
+
+char slave_Rx;
+void i2c_Slave_Receiver(void){
+	while(!I2C_CheckEvent(I2C2, I2C_EVENT_SLAVE_RECEIVER_ADDRESS_MATCHED));
+	while(!I2C_CheckEvent(I2C2, I2C_EVENT_SLAVE_BYTE_RECEIVED));
+	slave_Rx = I2C_ReceiveData(I2C2);
+	while(!I2C_CheckEvent(I2C2, I2C_EVENT_SLAVE_STOP_DETECTED));
 }
